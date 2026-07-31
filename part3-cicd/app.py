e0f6e6d7b79d7a18f681d2c3726a62a659b305d5
Akshitha <akshithareddy1906@gmail.com>
2026-07-31 19:50:23 +0530

    Fix flake8 newline warning

--- part3-cicd/app.py
+++ part3-cicd/app.py
@@ -26,3 +26,4 @@ def add(a, b):
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
--- part3-cicd/tests/test_app.py
+++ part3-cicd/tests/test_app.py
@@ -26,3 +26,4 @@ class AppTest(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+    
